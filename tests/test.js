7446722db04b23c5f77e01f2b921bfebec552e4a
Nish Shukla <nishith.shukla@arvatosystems.com>
2020-10-28 14:13:49 -0400

    changed the test

--- tests/test.js
+++ tests/test.js
@@ -8,7 +8,6 @@ describe('Tests index', function() {
     index.getContacts({ /* event */ }, { /* context */ }, (err, result) => {
       try {
         test.number(result.statusCode).is(200);
-        test.string(result.body).contains('Congratulations');
         test.value(result).hasHeader('content-type', 'text/html');
         done();
       } catch(error) {
